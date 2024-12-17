53836c6f6c3334c80cecbfffc3ff908b639ea60b
devnok <ha819ha.dev@gmail.com>
2024-12-17 21:33:49 +0900

    android 빌드 설정 변경

--- ios/Pods/Target Support Files/Pods-Aiming-AimingTests/Pods-Aiming-AimingTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Aiming_AimingTests : NSObject
-@end
-@implementation PodsDummy_Pods_Aiming_AimingTests
-@end
--- ios/Pods/Target Support Files/Pods-Aiming/Pods-Aiming-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Aiming : NSObject
-@end
-@implementation PodsDummy_Pods_Aiming
-@end
